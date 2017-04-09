51cee22dd1ca451cec49bc65c3c6c0e0bba27e2b
Zach Mitchell <zjmitche@gmail.com>
2017-04-09 11:57:34 -0600

    Side navigation fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
 $(document).ready(function() {
     $(window).scroll(function() {
         var y_scroll_pos = window.pageYOffset;
-        var scroll_pos_test = 100;
+        var scroll_pos_test = 0;
         if (y_scroll_pos > scroll_pos_test) {
-            $('#textLeft, #textRight, #iconsLeft a').css('color', '#52796F');
-            $('nav').css('display', 'none');
-            $('.wrapper .overlay').css('opacity', '.8');
+            $('#textRight').css({'background': '#52796F', 'margin': '0', 'bottom': '19%'});
+            $('#textLeft').css('display', 'none');
+            $('.brandLeft').css('display', 'block');
+            $('#menuRight').css('display', 'block');
+
+            $('#iconsLeft li').css('margin', '0 0 17px 0');
+            $('#iconsLeft li:first-child').css({'background': 'hsl(221, 44%, 41%)'});
+            $('#iconsLeft li:nth-child(2)').css({'background': 'hsl(195, 100%, 60%)'});
+            $('#iconsLeft li:nth-child(3)').css({'background': 'hsl(7, 71%, 55%)'});
+            $('#iconsLeft li:last-child').css({'background': 'hsl(199, 85%, 36%)'});
+
+            $('nav ul, nav button').addClass('animated fadeOutUp');
+            $('.wrapper .overlay').css('opacity', '.9');
         } else {
-            $('#textLeft, #textRight, #iconsLeft a').css('color', 'white');
-            $('nav').css('display', 'block');
-            $('.wrapper .overlay').css('opacity', '.3');
+            $('#textRight').css({'background': 'none', 'margin': '0 20px 0 0', 'bottom': '23%'});
+            $('#textLeft').css('display', 'block');
+            $('.brandLeft').css('display', 'none');
+            $('#menuRight').css('display', 'none');
+
+            $('#iconsLeft li').css('margin', '0 0 30px 9px');
+            $('#iconsLeft li:first-child').css({'background': 'none'});
+            $('#iconsLeft li:nth-child(2)').css({'background': 'none'});
+            $('#iconsLeft li:nth-child(3)').css({'background': 'none'});
+            $('#iconsLeft li:last-child').css({'background': 'none'});
+
+            $('nav ul, nav button').removeClass('animated fadeOutUp').addClass('animated fadeInDown');
+            $('.wrapper .overlay').css('opacity', '.2');
         }
     });
 
